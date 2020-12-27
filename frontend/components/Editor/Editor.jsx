297a8daa49fff5f2cf9c52b5271b3a03db9474e7
KJ Shanks <kyle.j.shanks@gmail.com>
2020-12-27 01:35:20 -0500

    Add better tab functionality and continue fixing look and feel

--- frontend/components/Editor/Editor.jsx
+++ frontend/components/Editor/Editor.jsx
@@ -3,33 +3,83 @@ import PropTypes from 'prop-types';
 import Prism from "prismjs";
 import { ComponentContainer, TextArea, CodeDisplay } from './styles.js';
 
+/* TODO: Quality of life things
+    - On keep indentation of the current line when adding a new line
+    - On backspace, if the selectionStart and selectionEnd are the same and
+      the 4 chars before are a tab, delete the tab
+    - On [, {, (, ", ', or `, automatically add the other half
+    - Add a system for undos and redos, they are p broken right now
+*/
+
+const TAB = '    ';
+
 const Editor = ({ className, initialContent, language }) => {
     const BASE_CLASS_NAME = 'Editor';
     const [content, setContent] = useState(initialContent);
     const [editorHeight, setEditorHeight] = useState(320);
-    const textareaRef = useRef();
     const outputRef = useRef();
 
-    const handleKeyDown = (e) => {
-        // Handle tabs
-        if (e.key === 'Tab') {
-            e.preventDefault();
+    const getLines = () => content.split("\n");
 
-            const ref = e.currentTarget;
-            const pos = ref.selectionStart;
-            setContent(`${content.substring(0, pos)}    ${content.substring(pos, content.length)}`);
+    // Get the line numbers for the current selection
+    const getSelectionLines = (start, end) => {
+        const lines = getLines();
+        const selectionRange = [null, null];
+        let total = 0;
 
-            // Janky way to cursor move to the correct position after the state update
-            setTimeout(() => {
-                ref.selectionStart = pos + 4;
-                ref.selectionEnd = pos + 4;
-            }, 5);
+        for (let i = 0; i < lines.length; i++) {
+            total += lines[i].length + 1;
+            if (selectionRange[0] === null && total >= start) selectionRange[0] = i;
+            if (selectionRange[1] === null && total >= end) selectionRange[1] = i;
         }
+
+        return selectionRange;
+    };
+
+    const handleTabActions = (e) => {
+        e.preventDefault();
+        const ref = e.currentTarget;
+        const start = ref.selectionStart;
+        const end = ref.selectionEnd;
+        const lines = getLines();
+        let startShift = 4;
+        let endShift = 0;
+
+        if (e.shiftKey) {
+            startShift = 0;
+            const selectRange = getSelectionLines(start, end);
+            for (let i = selectRange[0]; i < selectRange[1] + 1; i++) {
+                if (lines[i].slice(0, 4) === TAB) {
+                    lines[i] = lines[i].slice(4);
+                    i === selectRange[0] ? startShift = -4 : endShift -= 4;
+                }
+            }
+            setContent(lines.join("\n"));
+        } else if (start !== end) {
+            const selectRange = getSelectionLines(start, end);
+            for (let i = selectRange[0]; i < selectRange[1] + 1; i++) {
+                lines[i] = `${TAB}${lines[i]}`;
+                if (i !== selectRange[0]) endShift += 4;
+            }
+            setContent(lines.join("\n"));
+        } else {
+            setContent(`${content.substring(0, end)}${TAB}${content.substring(end, content.length)}`);
+        }
+
+        // Janky way to move selection start end end to the correct position after the update
+        setTimeout(() => {
+            ref.selectionStart = start + startShift;
+            ref.selectionEnd = end + startShift + endShift;
+        }, 0);
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Tab') handleTabActions(e);
     };
 
     useEffect(() => {
         Prism.highlightAll();
-        if (outputRef.current && outputRef.current.clientHeight !== editorHeight) {
+        if (outputRef.current.clientHeight !== editorHeight) {
             setEditorHeight(outputRef.current.clientHeight);
         }
     }, [language, content]);
@@ -38,15 +88,15 @@ const Editor = ({ className, initialContent, language }) => {
         <ComponentContainer className={`${BASE_CLASS_NAME} ${className}`.trim()}>
             <TextArea
                 className={`${BASE_CLASS_NAME}__Input`}
-                ref={textareaRef}
+                style={{ height: editorHeight }}
                 value={content}
                 onChange={e => setContent(e.target.value)}
                 onKeyDown={handleKeyDown}
-                style={{height: editorHeight}}
-                spellcheck={false}
+                spellCheck={false}
             />
             <CodeDisplay className={`${BASE_CLASS_NAME}__Output`} ref={outputRef}>
                 <code className={`language-${language}`}>{content}</code>
+                {/* Added so that an empty line at the end will update height */}
                 <br/>
             </CodeDisplay>
         </ComponentContainer>
--- frontend/components/Editor/styles.js
+++ frontend/components/Editor/styles.js
@@ -6,20 +6,19 @@ export const ComponentContainer = styled.div`
     height: 20rem;
     overflow: auto;
     width: calc(100vw - 2rem);
-    max-width: 60rem;
+    max-width: 40rem;
     margin: 1rem auto;
     background: #1c252c;
-	border-radius: 0.5rem;
+    border-radius: 0.5rem;
     box-shadow: 0 2px 10px 1px rgba(0,0,0,0.35);
 `;
 
 const editorStyles = `
-    color: #d4d4d4;
-	font-size: 13px;
-	text-shadow: none;
-	font-family: Menlo, Monaco, Consolas, "Andale Mono", "Ubuntu Mono", "Courier New", monospace;
-	white-space: pre-wrap;
-	word-spacing: normal;
+    font-size: 13px;
+    text-shadow: none;
+    font-family: Menlo, Monaco, Consolas, "Andale Mono", "Ubuntu Mono", "Courier New", monospace;
+    white-space: pre-wrap;
+    word-spacing: normal;
     line-height: 1.5;
     overflow: hidden;
 `;
@@ -29,24 +28,32 @@ export const TextArea = styled.textarea`
     position: absolute;
     min-height: 100%;
     width: 100%;
-    padding: 1rem 2rem;
+    padding: 1.5rem 2rem;
     resize: none;
+    color: #b7c5d3;
     ${editorStyles}
 
+    /* Hide the textarea content */
     background-color: transparent;
     border: none;
     outline: none;
     -webkit-text-fill-color: transparent;
+
+    &::selection {
+        text-shadow: none;
+        background-color: rgba(255,255,255,0.14);
+    }
 `;
 
 export const CodeDisplay = styled.pre`
     display: block;
     position: relative;
     min-height: 100%;
-    padding: 1rem 2rem;
+    padding: 1.5rem 2rem;
     ${editorStyles}
 
     & > code {
+        display: block;
         ${editorStyles}
     }
 `;
